c045b32a5a04d7b21725e1f6e312e90678666182
shockingblue211 <shockingblue211@gmail.com>
2018-06-03 10:34:45 -0400

    add print name

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ app.controller("ClockCtrl", Clock);
 function Clock($scope) {
     var date = new Date();
     $scope.curTime = date.toTimeString();
+    $scope.userName = "";
     $scope.updateTime = function(){
         console.log("Button clicked");
         var date = new Date();
