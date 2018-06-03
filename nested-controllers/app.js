c6549ad9972e7ea16d292af7089ecc1fd5ee0d41
shockingblue211 <shockingblue211@gmail.com>
2018-06-03 14:49:33 -0400

    controller as syntax

--- nested-controllers/app.js
+++ nested-controllers/app.js
@@ -3,10 +3,14 @@ var app = angular.module("nestedControllers", []);
 app.controller("Ctrl1", Ctrl1);
 app.controller("Ctrl2", Ctrl2);
 
-function Ctrl1($scope){
-    $scope.testProp = "From Ctrl 1";
+function Ctrl1(){
+    // when use controller as syntax, there is an explicit statement:
+    // var this = $scope
+    this.testProp = "From Ctrl 1";
 }
 
-function Ctrl2($scope){
-    //$scope.testProp = "From Ctrl 2";
+function Ctrl2(){
+    // when use controller as syntax, there is an explicit statement:
+    // var this = $scope
+    this.testProp = "From Ctrl 2"; 
 }
